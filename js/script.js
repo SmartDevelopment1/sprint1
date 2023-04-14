b93b0af36dee3de7407a0b2fcbfd297fbd414b75
MigsRS123 <rm551239@fiap.com.br>
2023-04-14 19:07:06 -0300

    branch 551239

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const h1Element = document.getElementsById('Quem somos')
+const h1Element = document.getElementsById ('Quem somos')
 
-h1Element.addElementListener('mouseover',()=>{
+h1Element.addElementListener('mouseover', ()=>{
     h1Element.style.color = "#808080";
 })
 h1Element.addElementListener('mouseout',()=>{
