57ed40cc8bcf460aa2fb7f0298239b9d803c9c3a
PedroPereiradosSantos37 <rm552047@fiap.com.br>
2023-04-14 18:21:19 -0300

    Sprint 1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const h1Element = document.getElementById ("Soluções")
+
+h1Element.addEventListener("mouseover", ()=>{
+    h1Element.style.color = "#808080";
+})
+
+h1Element.addEventListener("mouseout", ()=>{
+   h1Element.style.color = "#000000";
+})
+
+h1Element.addEventListener("mouseout", alteraCorFundo)
